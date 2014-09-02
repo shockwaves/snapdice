99b15d0b4065951cc76621459d3848d9b9795379
shockwaves <shockwaves@shockwaves-System-Product-Name>
2014-09-02 23:37:46 +0300

    Статистика частоты граней Дубли шансов в розыгрыше, бросков игрока, подряд - достижение (x1, x2, x3....)
    Испраленные Баги
    - не меняется имя игрока в розыгрыше

--- models/app.js
+++ models/app.js
@@ -1,46 +1,60 @@
 app = {
-	hits: 0,
-	time: 0,
-	score: [],
-	drops: 0,
-	doubles: 0,
-	state: false,
-	
-	start: function() {
-		this.state = true;
-		this.time = $.now();
-	},
-	
-	hit: function() {
-		return this.hits++;
-	},
-	
-	drop: function(left, right) {
-		this.drops++; 
-		this.score.push(left, right); 
-		if(left == right) this.doubles++;
-	},
-	
-	results: function() {
-		return {
-			hits: 		this.hits,
-			drops: 		this.drops,
-			doubles: 	this.doubles,
-			score: 		this.score.sum(),
-			time: 		(this.time) ? ($.now() - this.time).toHMS() : 0,
-			player1:	player.name(0),
-			player2:	player.name(1)
-		};
-	},
-	
-	reset: function() {
-		this.hits = 0;
-		this.time = 0;
-		this.drops = 0;
-		this.doubles = 0;
-		this.score = [];
-		player.all = [];
-		player.active = 0;
-		this.state = false;
-	}
+    hits: 0,
+    time: 0,
+    score: [],
+    face: [],
+    drops: 0,
+    doubles: 0,
+    state: false,
+    achievement: 0,
+    start: function() {
+        this.state = true;
+        this.time = $.now();
+        this.face[1] = 0;
+        this.face[2] = 0;
+        this.face[3] = 0;
+        this.face[4] = 0;
+        this.face[5] = 0;
+        this.face[6] = 0;
+    },
+    hit: function() {
+        return this.hits++;
+    },
+    drop: function(left, right) {
+        this.drops++;
+        this.face[left]++;
+        this.face[right]++;
+        this.score.push(left, right);    
+        if (left == right)
+            this.doubles++;
+    },
+    results: function() {
+        return {
+            hits: this.hits,
+            drops: this.drops,
+            doubles: this.doubles,
+            score: this.score.sum(),
+            time: (this.time) ? ($.now() - this.time).toHMS() : 0,
+            player1: player.name(0),
+            player2: player.name(1),
+            face1: this.face[1],
+            face2: this.face[2],
+            face3: this.face[3],
+            face4: this.face[4],
+            face5: this.face[5],
+            face6: this.face[6],
+        };
+    },
+    reset: function() {
+        this.hits = 0;
+        this.time = 0;
+        this.drops = 0;
+        this.doubles = 0;
+        this.score = [];
+        player.all = [];
+        player.active = 0;
+        this.state = false;
+        this.face = [];
+        this.achievement = 0;
+    }
 };
--- models/player.js
+++ models/player.js
@@ -1,50 +1,47 @@
 player = {
-	
-	data: [],
-	active: 0,
-	
-	add: function(data) {
-		this.all = data;
-	},
-	
+    data: [],
+    active: 0,
+    add: function(data) {
+        this.all = data;
+    },
 //	установка ID и имени активного игрока  
-	appoint: function(id) {
-		this.active = parseInt(id);
-	},
-	
+    appoint: function(id) {
+        this.active = parseInt(id);
+    },
 //	смена активного игрока
-	next: function() {
-		this.active = (this.active == 1) ? 0 : 1;
-		return this.active;
-	},
-
+    next: function() {
+        this.active = (this.active == 1) ? 0 : 1;
+        return this.active;
+    },
 //	сохранение имени
-	save: function(name) {
-		if(!name.length) 
-			return false;
-		if(this.data.length == 2)
-			this.data[this.active] = name;
-		else
-			this.data.push({name:name});
-		return true;
-	},
-	
-	get: function(id) {
-		if(typeof(id)==='undefined') 
-		id = this.active;
-		return this.data[id].name;
-	},
-	
-	first: function() {
-		return this.get(0);
-	},
-	
-	second: function() {
-		return this.get(1);
-	},
-	
-	name: function(index) {
-		return this.data[index];
-	}
-	
+    save: function(name) {
+        if (!name.length)
+            return false;
+        if (this.data.length == 2)
+            this.data[this.active].name = name;
+        else
+            this.data.push({name: name});
+        return true;
+    },
+    get: function(id) {
+        if (typeof (id) === 'undefined')
+            id = this.active;
+        return this.data[id].name;
+    },
+    first: function() {
+        return this.get(0);
+    },
+    second: function() {
+        return this.get(1);
+    },
+    name: function(id) {
+        if (typeof (id) === 'undefined')
+            id = this.active;
+        return (this.data[id]) ? this.data[id].name : false;
+    },
+    rewarding: function(id) {
+        if(this.data[id].achievement)
+        this.data[id].achievement++;
+        else this.data[id].achievement++;
+    }
 };
